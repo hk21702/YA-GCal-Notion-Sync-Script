4d424a74c09089bc54fdbeb4b9d2015a77d33c82
kat <28567881+hk21702@users.noreply.github.com>
2022-08-17 11:12:21 -0400

    delete gcal events when cancelled

--- main.js
+++ main.js
@@ -3,26 +3,29 @@ const DATE_NOTION = "Date";
 const TAGS_NOTION = "Tags";
 const DESCRIPTION_NOTION = "Description";
 
-const DELETION_NOTION = "Cleanup";
-
 const EVENT_ID_NOTION = "Event ID";
-const ON_GCAL_NOTION = "On GCal?";
+const CALENDAR_NAME_NOTION = "Calendar";
 const CALENDAR_ID_NOTION = "Calendar ID";
 const LAST_SYNC_NOTION = "Last Sync";
 
 const ARCHIVE_CANCELLED_EVENTS = false;
+const DELETE_CANCELLED_EVENTS = true;
 
 const CANCELLED_TAG_NAME = "Cancelled/Removed";
-const CANCELLED_TAG_COLOR = "red";
 
 function main() {
   parseNotionProperties();
-  for (const [c_name, c_id] of Object.entries(CALENDAR_IDS)) {
-    logSyncedEvents(c_name, c_id, false);
+
+  if (DELETE_CANCELLED_EVENTS) {
+    deleteCancelledEvents();
+  }
+
+  for (var c_name of Object.keys(CALENDAR_IDS)) {
+    logSyncedEvents(c_name, false);
   }
 }
 
-function logSyncedEvents(calendar_name, calendar_id, fullSync) {
+function logSyncedEvents(c_name, fullSync) {
   let properties = PropertiesService.getUserProperties();
   let options = {
     maxResults: 20,
@@ -42,7 +45,7 @@ function logSyncedEvents(calendar_name, calendar_id, fullSync) {
   do {
     try {
       options.pageToken = pageToken;
-      events = Calendar.Events.list(calendar_id, options);
+      events = Calendar.Events.list(CALENDAR_IDS[c_name], options);
     } catch (e) {
       // Check to see if the sync token was invalidated by the server;
       // if so, perform a full sync instead.
@@ -50,18 +53,20 @@ function logSyncedEvents(calendar_name, calendar_id, fullSync) {
         e.message === "Sync token is no longer valid, a full sync is required."
       ) {
         properties.deleteProperty("syncToken");
-        logSyncedEvents(calendar_id, true);
+        logSyncedEvents(CALENDAR_IDS[c_name], true);
         return;
       } else {
         throw new Error(e.message);
       }
     }
 
+    events["c_name"] = c_name;
+
     if (events.items && events.items.length > 0) {
-      console.log("Parsing new events. %s", calendar_name);
+      console.log("Parsing new events. %s", c_name);
       parseEvents(events);
     } else {
-      console.log("No events found. %s", calendar_name);
+      console.log("No events found. %s", c_name);
     }
 
     pageToken = events.nextPageToken;
@@ -76,6 +81,7 @@ function logSyncedEvents(calendar_name, calendar_id, fullSync) {
 function parseEvents(events) {
   for (let i = 0; i < events.items.length; i++) {
     let event = events.items[i];
+    event["c_name"] = events["c_name"];
     if (event.status === "cancelled") {
       console.log("Event id %s was cancelled.", event.id);
       // Remove the event from the database
@@ -99,7 +105,7 @@ function parseEvents(events) {
         end = event.end.dateTime;
         console.log("%s (%s)", event.summary, start.toLocaleString());
       }
-      page_response = getExistingPage(event);
+      page_response = getPageFromEvent(event);
       if (page_response) {
         console.log("Database event exists but requires update.");
         updateDatabaseEntry(
@@ -126,6 +132,7 @@ function updateDatabaseEntry(event, page_id, existing_tags = []) {
   payload["properties"] = convertToNotionProperty(event, existing_tags);
 
   if (ARCHIVE_CANCELLED_EVENTS && event.status === "cancelled") {
+    console.log("Archiving cancelled event.");
     payload["archived"] = true;
   }
 
@@ -146,7 +153,7 @@ function createDatabaseEntry(event) {
 
   payload["properties"] = convertToNotionProperty(event);
 
-  const responseData = notionFetch(url, payload, "GET");
+  const responseData = notionFetch(url, payload, "POST");
 }
 
 /**
@@ -154,7 +161,7 @@ function createDatabaseEntry(event) {
  * @param {String} event
  * @returns {}
  */
-function getExistingPage(event) {
+function getPageFromEvent(event) {
   const url = getDatabaseURL();
   const payload = {
     filter: {
@@ -184,21 +191,37 @@ function getExistingPage(event) {
 }
 
 /**
+ * Retrieve notion page using page id
+ * @param {String} page_id
+ * @returns {}
+ */
+function getPageProperty(page_id, property_id) {
+  const url =
+    "https://api.notion.com/v1/pages/" + page_id + "/properties/" + property_id;
+  const responseData = notionFetch(url, null, "GET");
+  return responseData;
+}
+
+/**
  * Interact with notion API
  * @param {*} url
  * @param {*} payload
  * @param {*} method
  * @returns
  */
-
 function notionFetch(url, payload, method = "POST") {
   // UrlFetchApp is sync even if async is specified
-  const response = UrlFetchApp.fetch(url, {
+  let options = {
     method: method,
     headers: getNotionHeaders(),
-    payload: JSON.stringify(payload),
     muteHttpExceptions: true,
-  });
+  };
+
+  if (payload) {
+    options["payload"] = JSON.stringify(payload);
+  }
+
+  const response = UrlFetchApp.fetch(url, options);
 
   if (response.getResponseCode() === 200) {
     const responseData = JSON.parse(response.getContentText());
@@ -265,49 +288,29 @@ function convertToNotionProperty(event, existing_tags = []) {
         },
       ],
     },
-    [ON_GCAL_NOTION]: {
-      type: "checkbox",
-      checkbox: true,
-    },
-    /**CALENDAR_ID_NOTION: {
-      rich_text: [
-        {
-          text: {
-            content: gCal_calendarId[i],
-          },
-        },
-      ],
-    },
-    CALENDAR: {
+    [CALENDAR_ID_NOTION]: {
       select: {
-        name: gCal_calendarName[i],
+        name: CALENDAR_IDS[event.c_name],
       },
-    }, */
+    },
+    [CALENDAR_NAME_NOTION]: {
+      select: {
+        name: event.c_name,
+      },
+    },
   };
 
-  if (event.description) {
-    property[DESCRIPTION_NOTION] = {
-      type: "rich_text",
-      rich_text: [
-        {
-          text: {
-            content: event.description,
-          },
+  property[DESCRIPTION_NOTION] = {
+    type: "rich_text",
+    rich_text: [
+      {
+        text: {
+          content: event.description ? event.description : "",
         },
-      ],
-    };
-  } else {
-    property[DESCRIPTION_NOTION] = {
-      type: "rich_text",
-      rich_text: [
-        {
-          text: {
-            content: "",
-          },
-        },
-      ],
-    };
-  }
+      },
+    ],
+  };
+
   if (event.start) {
     let start_time;
     let end_time;
@@ -333,19 +336,17 @@ function convertToNotionProperty(event, existing_tags = []) {
       },
     };
 
-    if (event.summary) {
-      property[NAME_NOTION] = {
-        type: "title",
-        title: [
-          {
-            type: "text",
-            text: {
-              content: event.summary,
-            },
+    property[NAME_NOTION] = {
+      type: "title",
+      title: [
+        {
+          type: "text",
+          text: {
+            content: event.summary ? event.summary : "",
           },
-        ],
-      };
-    }
+        },
+      ],
+    };
   }
 
   if (event.status === "cancelled") {
@@ -414,20 +415,15 @@ function handleEventCancelled(event) {
 
   if (page_id) {
     updateDatabaseEntry(event, page_id);
-  } else if (!ARCHIVE_CANCELLED_EVENTS) {
-    createDatabaseEntry(event);
   } else {
-    console.log(
-      "Event %s not found in Notion database. Archive is enabled. Skipping.",
-      event.id
-    );
+    console.log("Event %s not found in Notion database. Skipping.", event.id);
   }
 }
 
 /** Update database structure */
 function updateDatabaseStructure() {
   const url = getDatabaseURL();
-  const response = notionFetch(url, {}, "GET");
+  const response = notionFetch(url, null, "GET");
 
   if (response.properties[TAGS_NOTION]) {
     if (response.properties[TAGS_NOTION].type === "multi_select") {
@@ -442,4 +438,53 @@ function updateDatabaseStructure() {
   }
 
   notionFetch(url, payload, "POST");
+}
+
+/** Delete events marked as cancelled in gcal */
+async function deleteCancelledEvents() {
+  const url = getDatabaseURL();
+  const payload = {
+    filter: {
+      property: TAGS_NOTION,
+      multi_select: { contains: CANCELLED_TAG_NAME },
+    },
+  };
+  const response_data = notionFetch(url, payload, "POST");
+
+  for (let i = 0; i < response_data.results.length; i++) {
+    let result = response_data.results[i];
+
+    let last_sync_date = getPageProperty(
+      result.id,
+      result.properties[LAST_SYNC_NOTION].id
+    );
+
+    let calendar_id = getPageProperty(
+      result.id,
+      result.properties[CALENDAR_ID_NOTION].id
+    );
+
+    calendar_id = calendar_id.select.name;
+
+    let event_id = getPageProperty(
+      result.id,
+      result.properties[EVENT_ID_NOTION].id
+    );
+
+    event_id = event_id.results[0].rich_text.plain_text;
+
+    if (
+      new Date(last_sync_date.date.start) < new Date(result.last_edited_time)
+    ) {
+      console.log("Deleting event %s from gCal", event_id);
+
+      try {
+        let calendar = CalendarApp.getCalendarById(calendar_id);
+        await calendar.getEventById(event_id).deleteEvent();
+      } catch (e) {
+        console.log(e);
+        continue;
+      }
+    }
+  }
 }
