e744467f69f065a8babc05bfaf37c6b2f296ebfd
kat <28567881+hk21702@users.noreply.github.com>
2022-08-19 22:22:06 -0400

    Instructions for calId

--- calendarIds.js
+++ calendarIds.js
@@ -1,3 +1,7 @@
 const CALENDAR_IDS = {
+  /* Add calendars here. 
+  The key (string before ':') is what you name the calendar. 
+  The value (string after ':') is the calendar ID. 
+  E.g. "My calendar name"]: "mycalendarid", */
   ["Primary"]: "primary",
 };
